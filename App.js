2aea43bd3bd9b322bb6d94bec4b97e1925004de3
Tumelodee <129734510+Tumelodee@users.noreply.github.com>
2023-10-11 10:27:10 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,159 +1,160 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { FontAwesome5 } from '@expo/vector-icons';
-import { Menu, Divider, Provider } from 'react-native-paper';
+import { View, Text, FlatList, TextInput, Button, StyleSheet, SafeAreaView, TouchableOpacity, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const HomePage = ({navigation}) => {
-  
-  const [isMenuVisible, setMenuVisible] = useState(false);
+const tutorsData = [
+  { id: '1', name: 'Tutor 1', subject: 'Math', level: 'Intermediate', bio: 'Experienced math tutor with a passion for teaching.', rating: 4.5 },
+  { id: '2', name: 'Tutor 2', subject: 'Science', level: 'Advanced', bio: 'Science enthusiast with years of teaching experience.', rating: 4.0 },
+  { id: '3', name: 'Tutor 3', subject: 'English', level: 'Beginner', bio: 'Dedicated English tutor focused on building strong language skills.', rating: 4.2 },
+  { id: '4', name: 'Tutor 4', subject: 'History', level: 'Advanced', bio: 'Passionate historian eager to share knowledge about the past.', rating: 4.7 },
+  { id: '5', name: 'Tutor 5', subject: 'Programming', level: 'Advanced', bio: 'Experienced programmer helping students master coding concepts.', rating: 4.9 },
+  { id: '6', name: 'Tutor 6', subject: 'Art', level: 'Intermediate', bio: 'Creative art tutor inspiring students to explore their artistic talents.', rating: 4.3 },
+  { id: '7', name: 'Tutor 7', subject: 'Music', level: 'Beginner', bio: 'Passionate music tutor offering lessons in various instruments and music theory.', rating: 4.8 },
+  { id: '8', name: 'Tutor 8', subject: 'Physics', level: 'Advanced', bio: 'Physics expert with a knack for simplifying complex concepts.', rating: 4.6 },
+  { id: '9', name: 'Tutor 9', subject: 'Chemistry', level: 'Intermediate', bio: 'Chemistry enthusiast dedicated to making chemistry fun and understandable.', rating: 4.4 },
+  { id: '10', name: 'Tutor 10', subject: 'Biology', level: 'Advanced', bio: 'Biology teacher with a focus on hands-on learning and practical experiments.', rating: 4.2 },
+  // Add more tutors as needed
+];
 
-  const openMenu = () => setMenuVisible(true);
-  const closeMenu = () => setMenuVisible(false);
+
+const Stack = createNativeStackNavigator();
+
+const TutorListScreen = ({ navigation }) => {
+  const [selectedModule, setSelectedModule] = useState('');
+  const [selectedLevel, setSelectedLevel] = useState('');
+  const [filteredTutors, setFilteredTutors] = useState(tutorsData); // Initialize with all tutors
+
+  const handleApplyFilters = () => {
+    const filteredTutors = tutorsData.filter(tutor => {
+      return (
+        (selectedModule === '' || tutor.subject.toLowerCase() === selectedModule.toLowerCase()) &&
+        (selectedLevel === '' || tutor.level === selectedLevel)
+      );
+    });
+
+    if (filteredTutors.length === 0) {
+      Alert.alert('No Tutors Found', 'There are no tutors available for the specified module or level.');
+    } else {
+      setFilteredTutors(filteredTutors);
+    }
+  };
+
+  const handleSelectTutor = tutor => {
+    navigation.navigate('TutorProfile', { tutor });
+  };
 
   return (
-    
-    <Provider>
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <TouchableOpacity onPress={openMenu}>
-            <FontAwesome5 name="bars" size={24} color="black" style={styles.menuIcon} />
-          </TouchableOpacity>
-          <Text style={styles.headerTitle}>campus-Connect</Text>
-        </View>
-
-        <TouchableOpacity style={styles.section}>
-          <FontAwesome5 name="bus" size={24} color="blue" />
-          <Text style={styles.sectionTitle}>Transport</Text>
-          {/* Add content for Transport section */}
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.section}>
-          <FontAwesome5 name="chalkboard-teacher" size={24} color="green" />
-          <Text style={styles.sectionTitle}>Tutors</Text>
-          {/* Add content for Tutors section */}
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.section}>
-          <FontAwesome5 name="shopping-cart" size={24} color="grey" />
-          <Text style={styles.sectionTitle}>Buy</Text>
-          {/* Add content for Buy section */}
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.section}>
-          <FontAwesome5 name="handshake" size={24} color="orange" />
-          <Text style={styles.sectionTitle}>Sell</Text>
-          {/* Add content for Sell section */}
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.section}>
-          <FontAwesome5 name="bed" size={24} color="red" />
-          <Text style={styles.sectionTitle}>Accommodation</Text>
-          {/* Add content for Accommodation section */}
-        </TouchableOpacity>
-
-        <Menu
-          visible={isMenuVisible}
-          onDismiss={closeMenu}
-          anchor={
-            <FontAwesome5 name="" size={24} color="black" style={styles.menuIcon} />
-            
-          }
-        >
-          <Menu.Item
-            icon="cog"
-            title="messages"
-            onPress={() => {
-              closeMenu();
-            }}
-          />
-
-          <Menu.Item
-            icon="cog"
-            title="Settings"
-            onPress={() => {
-              // Add logic for Settings
-              closeMenu();
-            }}
-          />
-          <Menu.Item
-            icon="shopping-cart"
-            title="Cart"
-            onPress={() => {
-              // Add logic for Cart
-              closeMenu();
-            }}
-          />
-          <Menu.Item
-            icon="user"
-            title="Profile"
-            onPress={() => {
-              // Add logic for Profile
-              closeMenu();
-            }}
-          />
-          <Menu.Item
-            icon="calendar"
-            title="Appointment"
-            onPress={() => {
-              // Add logic for Appointment
-              closeMenu();
-            }}
-          />
-          <Divider />
-          <Menu.Item
-            icon="help-circle"
-            title="Help"
-            onPress={() => {
-              // Add logic for Help
-              closeMenu();
-            }}
-          />
-          <Menu.Item
-            icon="log-out"
-            title="Log Out"
-            onPress={() => {
-              // Add logic for Log Out
-              closeMenu();
-            }}
-          />
-        </Menu>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.filterContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Module"
+          value={selectedModule}
+          onChangeText={setSelectedModule}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Level"
+          value={selectedLevel}
+          onChangeText={setSelectedLevel}
+        />
+        <Button title="Apply Filters" onPress={handleApplyFilters} />
       </View>
-    </Provider>
+      <FlatList
+        data={filteredTutors}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={[
+              styles.tutorItem,
+            ]}
+            onPress={() => handleSelectTutor(item)}
+          >
+            <Text style={styles.tutorName}>{item.name}</Text>
+            <Text>{item.subject}</Text>
+            <Text>Rating: {item.rating}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
+  );
+};
+
+const TutorProfileScreen = ({ route }) => {
+  const { tutor } = route.params;
+
+  const handleBookTutor = () => {
+    Alert.alert(
+      'Confirm Booking',
+      `Are you sure you want to book ${tutor.name}?`,
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'Book',
+          onPress: () => {
+            // Perform booking logic here
+            console.log(`Booked ${tutor.name}`);
+            Alert.alert('Booking Successful', `You have successfully booked ${tutor.name}.`);
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.tutorName}>{tutor.name}</Text>
+      <Text>Subject: {tutor.subject}</Text>
+      <Text>Level: {tutor.level}</Text>
+      <Text>Bio: {tutor.bio}</Text>
+      {/* Book Tutor button on the tutor's profile */}
+      <Button title="Book Tutor" onPress={handleBookTutor} />
+    </SafeAreaView>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="TutorList" component={TutorListScreen} />
+        <Stack.Screen name="TutorProfile" component={TutorProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    
+    padding: 16,
   },
-  header: {
+  filterContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 20,
+    marginBottom: 16,
   },
-  menuIcon: {
-    marginRight: 10,
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginRight: 8,
   },
-  headerTitle: {
-    fontSize: 24,
-    fontWeight: 'bold',
+  tutorItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
   },
-  section: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#f2f2f2',
-    padding: 15,
-    marginBottom: 10,
-    borderRadius: 8,
-  },
-  sectionTitle: {
+  tutorName: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginLeft: 10,
   },
 });
 
-export default HomePage;
+export default App;
